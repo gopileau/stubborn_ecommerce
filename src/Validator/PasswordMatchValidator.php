2a8e7f2bbfffe854f7c10e33528e1b819f65806d
gopileau <vanessagonzalez99@hotmail.com>
2025-03-30 22:43:16 +0200

    Votre message de commit ici

--- src/Validator/PasswordMatchValidator.php
+++ src/Validator/PasswordMatchValidator.php
@@ -3,21 +3,16 @@ namespace App\Validator;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
-use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 class PasswordMatchValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
-        if (!$constraint instanceof PasswordMatch) {
-            throw new UnexpectedTypeException($constraint, PasswordMatch::class);
-        }
+        $form = $this->context->getRoot();
+        $password = $form->get('password')->getData();
+        $confirmPassword = $value;
 
-        // Récupérer les données du formulaire
-        $formData = $this->context->getRoot()->getData(); // Récupérer l'objet User
-
-        // Vérifiez si le mot de passe correspond
-        if ($formData->getPassword() !== $value) {
+        if ($password !== $confirmPassword) {
             $this->context->buildViolation($constraint->message)
                 ->addViolation();
         }
